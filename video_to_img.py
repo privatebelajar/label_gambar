c41f6c021729f5b0dd467c9c434d849ab8c5c9e7
mnuralfiansyah <mnuralfiansyah@gmail.com>
2023-12-28 21:29:00 +0800

    :hammer: x

--- video_to_img.py
+++ video_to_img.py
@@ -3,7 +3,9 @@ vidcap = cv2.VideoCapture('big_buck_bunny_720p_5mb.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
-  cv2.imwrite("gambar/frame%d.png" % count, image)     # save frame as JPEG file      
-  success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  count += 1
+    success,image = vidcap.read()
+
+    if count % 25 == 0
+        cv2.imwrite("gambar/frame%d.png" % count, image)     # save frame as JPEG file      
+        print('Read a new frame: ', success)
+        count += 1
