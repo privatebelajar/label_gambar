3a513cb57608d03e0c88bb18902593d24b63946f
ARYA DEVA RAJENDRA <aryadevarajendra@ARYAs-MacBook-Air.local>
2023-12-29 18:53:24 +0700

    create folder gambar and add image file

--- video_to_img.py
+++ video_to_img.py
@@ -1,11 +1,11 @@
 import cv2
-vidcap = cv2.VideoCapture('70.mp4')
+vidcap = cv2.VideoCapture('72.mp4')
 success,image = vidcap.read()
 count = 0
 while success:
     success,image = vidcap.read()
 
     if count % 25 == 0:
-        cv2.imwrite("gambar/frame%d.png" % count, image)     # save frame as JPEG file      
+        cv2.imwrite("gambar1/72_frame%d.png" % count, image)     # save frame as JPEG file      
         print('Read a new frame: ', success)
     count += 1
